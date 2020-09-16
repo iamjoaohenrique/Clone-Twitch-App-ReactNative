0956623b3113752bf7bc4684f39d3577648fc4e9
João Henrique Silva <40443481+iamjoaohenrique@users.noreply.github.com>
2020-09-15 22:48:52 -0300

    criação das telas do app

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,66 @@ import React from 'react';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import  colors from './styles/colors';
+
+import Following from './pages/Following';
+import ComingSoon from './pages/ComingSoon';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const Routes: React.FC = () => (
   <NavigationContainer>
     <Navigator>
-      <Screen name="Following" component={View} />
-      <Screen name="Discover" component={View} />
-      <Screen name="Browse" component={View} />
-      <Screen name="Esports" component={View} />
+      <Screen name="Following" component={Following} options={{
+        tabBarIcon:({ size, focused }) => {
+          return(
+            <Ionicons
+              name="md-heart"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+
+          )
+        }
+      }}/>
+      <Screen name="Discover" component={ComingSoon} options={{
+        tabBarIcon:({ size, focused }) => {
+          return(
+            <MaterialCommunityIcons
+              name="compass-outline"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+
+          )
+        }
+      }}/>
+      <Screen name="Browse" component={ComingSoon} options={{
+        tabBarIcon:({ size, focused }) => {
+          return(
+            <Ionicons
+              name="md-browsers"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+
+          )
+        }
+      }} />
+      <Screen name="Esports" component={ComingSoon} options={{
+        tabBarIcon:({ size, focused }) => {
+          return(
+            <MaterialCommunityIcons
+              name="trophy-outline"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+
+          )
+        }
+      }}/>
     </Navigator>
   </NavigationContainer>
 );
